8faed6086715ade4f97f6a6645807b0697fd7b6f
Sagiv23 <104361570+sagiv23@users.noreply.github.com>
2023-06-22 13:14:48 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,3 +3,23 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     swal("Wait!", "Please rotate your screen to landscape mode for the best viewing experience!", "error");
   }); 
 }
+
+function sl_qu() {
+  document.getElementById("_qu").style.display = 'block';
+  document.getElementById("_qu").scrollIntoView({ behavior: 'smooth'});
+}
+
+function qu_sl() {
+  document.getElementById("sl").style.display = 'block';
+  document.getElementById("sl").scrollIntoView({ behavior: 'smooth'});
+}
+
+function qu_w() {
+  document.getElementById("_w").style.display = 'block';
+  document.getElementById("_w").scrollIntoView({ behavior: 'smooth'});
+}
+
+function w_sl() {
+  document.getElementById("sl").style.display = 'block';
+  document.getElementById("sl").scrollIntoView({ behavior: 'smooth'});
+}
