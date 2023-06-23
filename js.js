ae1c78e7a7b83c14e5a3e50631d89c172dc2ade2
Sagiv23 <104361570+sagiv23@users.noreply.github.com>
2023-06-23 14:40:30 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -9,17 +9,50 @@ function sl_qu() {
   document.getElementById("_qu").scrollIntoView({ behavior: 'smooth'});
 }
 
-function qu_sl() {
-  document.getElementById("sl").style.display = 'block';
-  document.getElementById("sl").scrollIntoView({ behavior: 'smooth'});
-}
-
 function qu_w() {
   document.getElementById("_w").style.display = 'block';
   document.getElementById("_w").scrollIntoView({ behavior: 'smooth'});
 }
 
-function w_sl() {
+function w_qu() {
+  document.getElementById("_qu").style.display = 'block';
+  document.getElementById("_qu").scrollIntoView({ behavior: 'smooth'});
+}
+
+function w_url(){
+  document.getElementById("_url").style.display = 'block';
+  document.getElementById("_url").scrollIntoView({ behavior: 'smooth'});
+}
+
+function url_w() {
+  document.getElementById("_w").style.display = 'block';
+  document.getElementById("_w").scrollIntoView({ behavior: 'smooth'});
+}
+
+function w_th(){
+  document.getElementById("_th").style.display = 'block';
+  document.getElementById("_th").scrollIntoView({ behavior: 'smooth'});
+}
+
+function th_url() {
+  document.getElementById("_url").style.display = 'block';
+  document.getElementById("_url").scrollIntoView({ behavior: 'smooth'});
+}
+
+function video_th() {
+  document.getElementById("_th").style.display = 'block';
+  document.getElementById("_th").scrollIntoView({ behavior: 'smooth'});
+}
+
+function th_video() {
+  document.getElementById("_video").style.display = 'block';
+  document.getElementById("_video").scrollIntoView({ behavior: 'smooth'});
+}
+
+
+
+
+function sl() {
   document.getElementById("sl").style.display = 'block';
   document.getElementById("sl").scrollIntoView({ behavior: 'smooth'});
 }
